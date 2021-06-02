e15f19d183b35a0e0064fcae344364608d0aa0a9
JamesZ <jiayang_zhao@ucsb.edu>
2021-06-02 14:53:21 +0800

    Update rect.h

--- rect.h
+++ rect.h
@@ -39,7 +39,7 @@ class Rect : public shape {
 		{
 			if(upperL.x()< 0 || upperL.y()< 0||lowerR.x()< 0 || lowerR.y()< 0)
 			{
-				std::cout << "rect vert less zero"<< endl;
+				std::cout << "rect vert less zero"<< std::endl;
 			}
 		}
 		catch(const std::invalid_argument& e)
@@ -51,7 +51,7 @@ class Rect : public shape {
 		{
 			if(upperL.x()> lowerR.x() || upperL.y()> lowerR.y())
 			{
-				std::cout <<"rect order incorrect"<< endl;
+				std::cout <<"rect order incorrect"<< std::endl;
 			}
 		}
 		catch(const std::invalid_argument& e)
